f3b7e0c442aa2e68a842bfd51eaeeee4f8796529
munaibrahimn <munaa97@gmail.com>
2025-12-08 13:12:49 +0000

    completed VPC module with example

--- modules/aws/vpc/variables.tf
+++ modules/aws/vpc/variables.tf
@@ -1,1 +1,25 @@
-#Variables will be added here
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability zones to use for subnets"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for public subnets"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for private subnets"
+}
+
+variable "enable_nat_gateway" {
+  type        = bool
+  default     = true
+  description = "Whether to create a NAT gateway for private subnets"
+}
